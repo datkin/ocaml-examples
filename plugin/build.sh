7b9915c110bf1a0c7f0c08687e4d0e52eb8239b2
Drew Atkin <datkin@gmail.com>
2016-04-17 15:00:24 -0400

    start working on plugin

---
+++ plugin/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+stubs=embedded_compiler_stubs
+
+rm ${stubs}.c
+
+ocamlbuild \
+  -pkg ocaml_plugin \
+  plugin_intf.cmi
+
+ocaml-embed-compiler \
+  -cc $(which ocamlopt.opt) \
+  _build/plugin_intf.cmi \
+  $(ocamlfind query ocaml_plugin)/ocaml_plugin.cmi \
+  $(opam config var lib)/ocaml/pervasives.cmi \
+  -o ${stubs}.c
+
+gcc \
+  -I$(opam config var lib)/ocaml \
+  -c ${stubs}.c \
+  -o _build/${stubs}.o
+
+ocamlbuild \
+  -use-ocamlfind \
+  -pkg core \
+  -pkg async \
+  -pkg ocaml_plugin \
+  -tag 'ppx(ppx-jane -as-ppx)' \
+  -tag debug \
+  -tag thread \
+  main.native
---
+++ plugin/main.ml
@@ -0,0 +1,52 @@
+open Core.Std
+open Async.Std
+open Ocaml_plugin.Std
+
+module Plugin = Ocaml_dynloader.Make (struct
+  type t = (module Plugin_intf.S)
+  let t_repr = "Plugin_intf.S"
+  let univ_constr = Plugin_intf.univ_constr
+  let univ_constr_repr = "Plugin.univ_constr"
+end)
+
+let config = Plugin_cache.Config.create ~dir:"/tmp" ()
+
+(* If we specify this, start up should be faster on additional runs. *)
+let persistent_archive_dirpath = "/tmp/plugin-example-archive"
+
+let command =
+  Command.async_or_error
+    ~summary:"Continually load the plugin, print the message as it updates"
+    Command.Spec.(
+      empty
+      +> anon ("plugin.ml" %: file)
+    )
+    (fun file () ->
+      Ocaml_compiler.create ~use_cache:config ~persistent_archive_dirpath ()
+      >>=? fun (`this_needs_manual_cleaning_after ocaml_compiler) ->
+      let loader = Ocaml_compiler.loader ocaml_compiler in
+      Deferred.forever None (fun prev_md5 ->
+        let md5 = Some (Digest.file file |> Digest.to_hex) in
+        begin
+          if Option.equal String.equal prev_md5 md5
+          then Deferred.unit
+          else begin
+            Plugin.load_ocaml_src_files loader [file]
+            >>| function
+            | Error err -> eprintf !"%{Error#hum}\n%!" err
+            | Ok plugin ->
+              let module M = (val plugin : Plugin_intf.S) in
+              printf "%s\n%!" M.message
+          end
+        end
+        >>= fun () ->
+        Clock.after (sec 1.)
+        >>= fun () ->
+        return md5);
+      Deferred.create (fun ivar ->
+        Signal.handle Signal.terminating ~f:(fun (_ : Signal.t) ->
+          Ivar.fill ivar ()))
+      >>= fun () ->
+      Ocaml_compiler.clean ocaml_compiler)
+
+let () = Command.run command

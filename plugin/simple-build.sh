28712f4d0a068a995f43bc90055c1e986e30741e
Drew Atkin <datkin@gmail.com>
2016-04-17 17:21:19 -0400

    better working version of simple build

--- plugin/simple-build.sh
+++ plugin/simple-build.sh
@@ -9,10 +9,16 @@ opam switch 4.02.3 && eval $(opam config env)
 
 ocamlbuild -clean
 
-  #-ocamlopt "ocamlopt $(opam config var lib)/ocaml_plugin/ocaml_plugin_ocamlbuild.cmx" \
-  #-just-plugin \
-  #-verbose 3 \
+# The following options to `ocamlbuild` may be helpful...
+#   -just-plugin \
+#   -verbose 3 \
+
+# The top two flags are *just* for building myocamlbuild.ml with access to the
+# Ocaml_plugin_ocamlbuild module. I would imagine there's a better way, but I
+# haven't found it yet.
 ocamlbuild \
+  -lflag "-I" -lflag $(opam config var lib)/ocaml_plugin \
+  -lflag $(opam config var lib)/ocaml_plugin/ocaml_plugin_ocamlbuild.cmxa \
   -use-ocamlfind \
   -pkg core \
   -pkg async \

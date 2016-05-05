1351b73fa186c047606d7498fb382cb6f93067ad
Drew Atkin <datkin@gmail.com>
2016-05-04 22:49:51 -0400

    get a little fancier -- introduce a message format and use unpack buffer

--- async/main.ml
+++ async/main.ml
@@ -1,6 +1,21 @@
 open Core.Std
 open Async.Std
 
+type message = {
+  sender : string;
+  time : Time.t;
+  data : string;
+} [@@deriving bin_io, sexp]
+
+let write_message writer message =
+  Writer.write_bin_prot writer bin_writer_message message
+
+let iter_messages reader ~f =
+  Unpack_sequence.unpack_iter
+    ~from:(Reader reader)
+    ~using:(Unpack_buffer.create_bin_prot bin_reader_message)
+    ~f
+
 let server =
   Command.async
     ~summary:"The server"
@@ -12,9 +27,9 @@ let server =
       let bus_ro = Bus.read_only bus in
       Tcp.Server.create (Tcp.on_port port) (fun addr reader writer ->
         Core.Std.printf !"%{Socket.Address.Inet} connected\n%!" addr;
-        let subscriber = Bus.subscribe_exn bus_ro [%here] ~f:(Writer.write writer) in
-        Pipe.iter_without_pushback (Reader.pipe reader) ~f:(Bus.write bus)
-        >>= fun () ->
+        let subscriber = Bus.subscribe_exn bus_ro [%here] ~f:(write_message writer) in
+        iter_messages reader ~f:(Bus.write bus)
+        >>= fun (_ : _ Unpack_sequence.Unpack_iter_result.t) ->
         Bus.unsubscribe bus_ro subscriber;
         Core.Std.printf !"%{Socket.Address.Inet} disconnected\n%!" addr;
         Deferred.unit)
@@ -25,14 +40,23 @@ let server =
 let client =
   Command.async
     ~summary:"The client"
-    (Command.Spec.(empty +> anon ("host:port" %: (Arg_type.create Host_and_port.of_string))))
-    (fun host_and_port () ->
+    (Command.Spec.(
+      empty
+      +> flag "user" (optional string) ~doc:"name Name of user"
+      +> anon ("host:port" %: (Arg_type.create Host_and_port.of_string))
+    ))
+    (fun sender host_and_port () ->
+      let sender = Option.value sender ~default:(Core.Std.Unix.getlogin ()) in
       let host, port = Host_and_port.tuple host_and_port in
       Tcp.with_connection (Tcp.to_host_and_port host port) (fun _ reader writer ->
         Core.Std.printf "connected\n%!";
         Deferred.any [
-          Pipe.iter_without_pushback (Reader.pipe reader) ~f:(printf "got: %s\n%!");
-          Pipe.iter_without_pushback (Reader.lines (force Reader.stdin)) ~f:(Writer.write writer);
+          iter_messages reader ~f:(fun message ->
+            printf !"%{sexp:message} %{Time.Span}\n%!"
+              message (Time.diff (Time.now ()) message.time))
+          >>| ignore;
+          Pipe.iter_without_pushback (Reader.lines (force Reader.stdin))
+            ~f:(fun data -> write_message writer { sender; time = Time.now (); data })
         ]))
 
 let () =

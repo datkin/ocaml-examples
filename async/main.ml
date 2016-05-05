5cc9c4bc104281c6c7903243cdbe6a581a25d4aa
Drew Atkin <datkin@gmail.com>
2016-05-04 22:09:22 -0400

    improve the async echo server example a bit

--- async/main.ml
+++ async/main.ml
@@ -2,24 +2,25 @@ open Core.Std
 open Async.Std
 
 let server =
-  let bus : (_, read_write) Bus.t =
-    Bus.create [%here] Arity1 ~allow_subscription_after_first_write:true ~on_callback_raise:ignore
-  in
-  let bus_ro = Bus.read_only bus in
   Command.async
     ~summary:"The server"
     Command.Spec.(empty +> anon ("port" %: int))
     (fun port () ->
-      Tcp.Server.create (Tcp.on_port port) (fun _ reader writer ->
-        Core.Std.printf "new client\n%!";
+      let bus : (_, read_write) Bus.t =
+        Bus.create [%here] Arity1 ~allow_subscription_after_first_write:true ~on_callback_raise:ignore
+      in
+      let bus_ro = Bus.read_only bus in
+      Tcp.Server.create (Tcp.on_port port) (fun addr reader writer ->
+        Core.Std.printf !"%{Socket.Address.Inet} connected\n%!" addr;
         let subscriber = Bus.subscribe_exn bus_ro [%here] ~f:(Writer.write writer) in
         Pipe.iter_without_pushback (Reader.pipe reader) ~f:(Bus.write bus)
         >>= fun () ->
         Bus.unsubscribe bus_ro subscriber;
+        Core.Std.printf !"%{Socket.Address.Inet} disconnected\n%!" addr;
         Deferred.unit)
-      >>= fun (_ : _ Tcp.Server.t) ->
+      >>= fun server ->
       Core.Std.printf "listening\n%!";
-      Deferred.never ())
+      Tcp.Server.close_finished server)
 
 let client =
   Command.async
@@ -27,12 +28,12 @@ let client =
     (Command.Spec.(empty +> anon ("host:port" %: (Arg_type.create Host_and_port.of_string))))
     (fun host_and_port () ->
       let host, port = Host_and_port.tuple host_and_port in
-      Tcp.with_connection (Tcp.to_host_and_port host port) (fun _ reader writer -> 
+      Tcp.with_connection (Tcp.to_host_and_port host port) (fun _ reader writer ->
         Core.Std.printf "connected\n%!";
-        don't_wait_for (
+        Deferred.any [
           Pipe.iter_without_pushback (Reader.pipe reader) ~f:(printf "got: %s\n%!");
-        );
-        Pipe.iter_without_pushback (Reader.pipe (force Reader.stdin)) ~f:(Writer.write writer)))
+          Pipe.iter_without_pushback (Reader.pipe (force Reader.stdin)) ~f:(Writer.write writer);
+        ]))
 
 let () =
   Command.group

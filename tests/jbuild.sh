d1c7c8e346a6015d31b7111e5e96bb3b3662e7a6
Drew Atkin <datkin@gmail.com>
2017-01-01 20:57:21 -0500

    cleanup

--- tests/jbuild.sh
+++ tests/jbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 jbuilder bin/inline_test_runner.exe
-./_build/default/bin/inline_test_runner.exe inline-test-runner x
+./_build/default/bin/inline_test_runner.exe inline-test-runner example_lib
